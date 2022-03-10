a398c1977e9088272adda509c27a2e778ae592ec
FilipV <filip.vrba@seznam.cz>
2022-03-10 17:58:26 +0100

    commit from notebook

--- main.py
+++ main.py
@@ -16,5 +16,6 @@ def print_bye(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_bye('Jack')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
